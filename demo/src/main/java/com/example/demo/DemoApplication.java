3eeb9eb9ee71a55b2c7313550d3979cee31ae817
Felipe Lemos <felipe.lemos@zup.com.br>
2018-03-24 11:31:23 -0300

    projeto crud para servir como backend para uma aplicção com angular 5

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.entity.Car;
+import com.example.demo.repository.CarRepository;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+    ApplicationRunner init(CarRepository repository) {
+        return args -> {
+            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+                      "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+                Car car = new Car();
+                car.setName(name);
+                repository.save(car);
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ demo/src/main/java/com/example/demo/controller/CoolCarController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Car;
+import com.example.demo.repository.CarRepository;
+
+@RestController
+@RequestMapping("/car")
+public class CoolCarController {
+
+	@Autowired
+	private CarRepository carRepository;
+	
+	@GetMapping("/cool-cars")
+    public Collection<Car> coolCars() {
+        return carRepository.findAll().stream()
+                .filter(this::isCool)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isCool(Car car) {
+        return !car.getName().equals("AMC Gremlin") &&
+                !car.getName().equals("Triumph Stag") &&
+                !car.getName().equals("Ford Pinto") &&
+                !car.getName().equals("Yugo GV");
+    }
+}
